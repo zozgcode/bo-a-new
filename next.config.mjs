f9228eb77291aa9b56ee52641a824d3fedd0e831
sunshine-web-coder <webdev4gaming@gmail.com>
2024-07-02 11:03:27 +0100

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "i.imgur.com",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "imgur.com",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "basho.fueko.net",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
